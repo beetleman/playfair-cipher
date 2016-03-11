0905fa99ca0321af10e74e6cee6c4c8424f62763
Mateusz Probachta (Beetleman) <mateusz.probachta@gmail.com>
2016-03-11 18:50:55 +0100

    position transformations

--- src/playfair_cipher/core.cljs
+++ src/playfair_cipher/core.cljs
@@ -3,8 +3,6 @@
 
 
 (def default-key "!JKBKU KBKK LLkhvbV")
-(def utf-8-num (range 65535))
-(def utf-8-char (mapv char utf-8-num))
 
 ;; app state
 
@@ -12,12 +10,89 @@
 (add-watch app-state :logger #(-> %4 clj->js js/console.debug))
 
 ;; ------------------------
+;; table
+(def utf-8-num (range 65535))
+(def utf-8-char (mapv char utf-8-num))
+
+(defn create-char-vector [table key]
+  (let [key (vec key)
+        key-set (set key)
+        table (filterv #((complement contains?) key-set %) table)]
+    (into key table)))
+
+
+(defn create-table [v]
+  (partition (-> (count v) Math/sqrt int) v))
+
+;; ------------------------
+;; Playfair Cipher
+(defn index-of [coll x]
+  (js->clj (.indexOf (clj->js coll) x)))
+
+(defn split-word [word l]
+  "split `word' into pairs, if odd numer of leter ausing `l' character"
+  (let [word (vec word)
+        word (if (odd? (count word))
+               (conj word l)
+               (word))]
+    (partition 2 word)))
+
+
+(defn get-char-position [table char]
+  (first (filter
+          #(not= -1 (second %))
+          (map-indexed (fn [idx itm] [idx (index-of itm char)]) table))))
+
+(defn get-chars-positions [table splited-word]
+  (map
+   #(map (partial get-char-position table) %)
+   splited-word))
+
+
+(defn crypt-position* [max-idx position next-index-fn]
+  (let [[l1 l2] position
+        [l1-x l1-y] l1
+        [l2-x l2-y] l2
+        allowed-idexies (cycle (range max-idx))
+        next-index (fn [idx] (nth allowed-idexies
+                                  (next-index-fn idx)))]
+    (cond
+      (= l1-x l2-x)
+      [[l1-x (next-index l1-y)]
+       [l2-x (next-index l2-y)]]
+      (= l1-y l2-y)
+      [[(next-index l1-x) l1-y]
+       [(next-index l2-x) l2-y]]
+      :else
+      [[l1-x l2-y]
+       [l2-x l1-y]])))
+
+(defn encrypt-position [max-idx position]
+  (crypt-position* max-idx position inc))
+
+
+(defn decrypt-position [max-idx position]
+  (crypt-position* max-idx position dec))
+
+
+
+(defn crypt-positions* [table positions conv-fn]
+  (map (partial conv-fn (-> table first count))
+       positions))
+
+(defn encrypt-positions [table positions]
+  (crypt-positions* table positions encrypt-position))
+
+
+(defn decrypt-positions [table positions]
+  (crypt-positions* table positions decrypt-position))
+
+
 ;; views
 
 
 (defn input-onchange-fn [state target]
   (fn [event]
-    (-> (char 999) clj->js js/console.debug)
     (swap! state assoc target (-> event .-target .-value))))
 
 
--- test/main_test.cljs
+++ test/main_test.cljs
@@ -3,5 +3,75 @@
   (:require [cljs.test]
             [playfair-cipher.core :as core]))
 
+(def test-vector
+  [\1 \2 \3 \4
+   \q \w \e \r
+   \a \s \d \f
+   \z \x \c \v
+   \0])
+
+(def test-table
+  [[\1 \2 \3 \4]
+   [\q \w \e \r]
+   [\a \s \d \f]
+   [\z \x \c \v]])
+
+
 (deftest test-pass []
   (is (= 2 2)))
+
+
+(deftest create-char-vector []
+  (is (=
+       (core/create-char-vector [\x \a \g \1 \z] [\g \z])
+       [\g \z \x \a \1])))
+
+
+(deftest create-table []
+  (is (=
+       (core/create-table test-vector)
+       test-table)))
+
+(deftest index-of []
+  (is (=
+       (core/index-of test-vector \q)
+       4)))
+
+(deftest split-word []
+  (is (=
+       (core/split-word "1q2w3" "X")
+       [[\1 \q] [\2 \w] [\3 \X]])))
+
+(deftest get-char-position []
+  (is (=
+       (core/get-char-position test-table \q)
+       [1 0])))
+
+(deftest get-chars-positions []
+  (is (=
+       (core/get-chars-positions test-table [[\1 \s] [\2 \x]])
+       [[[0 0]
+         [2 1]]
+        [[0 1]
+         [3 1]]])))
+
+(deftest encrypt-position []
+  (is (=
+       (core/encrypt-position 4
+                              [[0 1]
+                               [3 1]])
+       [[1 1]
+        [0 1]])))
+
+
+(deftest encrypt-positions []
+  (is (=
+       (core/encrypt-positions test-table
+                               [[[0 0]
+                                 [2 1]]
+                                [[0 1]
+                                 [3 1]]])
+       [[[0 1]
+         [2 0]]
+        [[1 1]
+         [0 1]]])))
